f13c2592e3ebaf2cdf96c7bf074f78a0cc27613b
Julian S <juli23072@gmail.com>
2019-10-12 18:53:48 +0200

    added alpha types to dummy_vison_events

--- fight/effects/dummy_vision_event.py
+++ fight/effects/dummy_vision_event.py
@@ -1,13 +1,13 @@
 import pygame
 
 
-# render every event in fight
 class DummyEvent:
-    def __init__(self, duration, color, area):
+    def __init__(self, duration, color, area, type_="fade"):
         self.duration = duration
         self.color = color
         self.created = pygame.time.get_ticks()
         self.area = area
+        self.type = type_
 
     @property
     def is_active(self):
@@ -20,7 +20,7 @@ class DummyEvent:
 
     def draw(self, surface):
         now = pygame.time.get_ticks()
-        state = now - self.created
+        state = (now - self.created) / self.duration
 
         if isinstance(self.area, list):
             self._render_as_cells(surface, state)
@@ -28,7 +28,17 @@ class DummyEvent:
             self._render_as_object(surface, state)
 
     def _render_as_cells(self, surface, state):
-        alpha = int((1 - (state / self.duration)) * 250)
+        alpha = 255
+        if self.type == "fade":
+            alpha = int((1 - state) * 255)
+        elif self.type == "full":
+            alpha = 255
+        elif self.type == "half_fade":
+            if state >= 0.5:
+                alpha = int((1 - state) * 155) + 100
+            else:
+                alpha = int(state * 155) + 100
+
         for cell in self.area:
             cell.draw(surface, self.color, alpha=alpha)
 
